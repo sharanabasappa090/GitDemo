320ef16ac48e39ddcf066730959ed13f9bdd2c31
rajkumar.c <rajkumar.c@laptop100.mportalindia.com>
2015-02-19 11:35:45 +0530

    Properly resolved the index

--- Test/src/com/mportal/demo/HelloWorld.java
+++ Test/src/com/mportal/demo/HelloWorld.java
@@ -14,14 +14,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		System.out.println("king4");
-<<<<<<< HEAD
 		System.out.println("king7");
-=======
-		System.out.println("king6");
->>>>>>> branch 'master' of git@github.com:sharanabasappa090/GitDemo.git
-
-
-
 	}
 
 }
