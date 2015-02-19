edc69f72b479aa5022851ce2efe57bf6aa69dd1c
sharanabasappa.r <sharanabasappa.r@10.11.245.191>
2015-02-19 12:01:55 +0530

    12

--- Test/src/com/mportal/demo/HelloWorld.java
+++ Test/src/com/mportal/demo/HelloWorld.java
@@ -14,7 +14,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		System.out.println("king4");
-		System.out.println("king10");
+		System.out.println("king12");
 	}
 
 }
